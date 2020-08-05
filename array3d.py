923b5ecd138db12a6b8aeabbc12b0e8a84edc8c5
TraumaAI <mike@trauma.ai>
2020-08-04 23:16:52 -0400

    Added new file array3d. py \n This file contains the main functions for 3 Dimensional Array creation for CNN/RNN/LSTM/etc..

---
+++ array3d.py
@@ -0,0 +1,58 @@
+import numpy as np
+from numpy.core.numeric import roll
+
+def shift_array(arr, num, rolling=False, fill_value=-777):
+    #  num = (np.abs(num) - 1) * np.sign(num)
+
+    if rolling == True:
+        result = np.zeros_like(arr)
+        num = (np.abs(num) - 1) * np.sign(num)
+        if num > 0:
+            result[:num] = fill_value
+            result[num:] = arr[:-num]
+        elif num < 0:
+            result[num:] = fill_value
+            result[:num] = arr[-num:]
+        else:
+            result[:] = arr
+        result = result[result != fill_value]
+        return result[:-1]
+    else:
+        result = arr.reshape(-1, num, 1)
+        nonroll_target = []
+        for i in range(result.shape[0]):
+            nonroll_target.append(result[i][-1])
+
+        return np.array(nonroll_target)
+
+
+def convert_3d_array(data, n_time_steps, features, rolling=False):
+    """
+    ---
+    Arguments:
+        data:
+        n_time_steps:
+        features:
+    Returns:
+        NumPy 3D array formatted for LSTM analysis.
+    """
+    if rolling == False:
+        return data.reshape((-1, n_time_steps, features))
+    else:
+        rolling3d = []
+        for i in range(data.shape[0]-n_time_steps):
+            rolling3d.append(data[i:i+n_time_steps].reshape((-1, n_time_steps, features)))
+
+        rolling3d = np.array(rolling3d)
+        rolling3d = rolling3d.reshape((rolling3d.shape[0], n_time_steps, features))
+
+        return rolling3d
+
+
+def create_2d_target_array(arr, time_steps, rolling=False, features=1):
+    if np.abs(time_steps) <=1:
+        return arr
+    else:
+        target_arr = shift_array(arr, -time_steps, rolling)
+
+    return target_arr.reshape((-1, features))
