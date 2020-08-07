9ce6d43db635aba4a523debca665697b2445ea6f
TraumaAI <mike@trauma.ai>
2020-08-06 22:10:08 -0400

    added fixture to test sets

--- test_array3d.py
+++ test_array3d.py
@@ -1,5 +1,18 @@
 import array3d as a3d
 import numpy as np
+import pytest
+
+
+@pytest.fixture
+def labels():
+    data = np.genfromtxt('mock_data.csv', delimiter=',')
+    return data[:, -1]
+
+
+@pytest.fixture
+def features():
+    data = np.genfromtxt('mock_data.csv', delimiter=',')
+    return data
 
 
 def test_simple():
@@ -7,10 +20,6 @@ def test_simple():
     return
 
 
-def test_2d_non_rolling():
-    data = np.genfromtxt('mock_data.csv', delimiter=',')
-    two_dim = a3d.create_2d_target_array(data[:, -1], 10, True)
-    assert two_dim[0] == data[:, -1][9]
-
-
-
+def test_2d_non_rolling(labels):
+    two_dim = a3d.create_2d_target_array(labels, 10, True)
+    assert two_dim[0] == labels[9]
