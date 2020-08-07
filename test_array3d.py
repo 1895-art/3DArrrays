a833f800261e899bc2e00835629666081d6e3dfb
TraumaAI <mike@trauma.ai>
2020-08-06 21:53:08 -0400

    added 2d array test

--- test_array3d.py
+++ test_array3d.py
@@ -1,3 +1,16 @@
+import array3d as a3d
+import numpy as np
+
+
 def test_simple():
     assert True
     return
+
+
+def test_2d_non_rolling():
+    data = np.genfromtxt('mock_data.csv', delimiter=',')
+    two_dim = a3d.create_2d_target_array(data[:, -1], 10, True)
+    assert two_dim[0] == data[:, -1][9]
+
+
+
