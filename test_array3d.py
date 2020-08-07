dae5d2ce9e099f8a6c3b556500c9bd3122be7df1
TraumaAI <mike@trauma.ai>
2020-08-06 23:33:22 -0400

    added more tests begining to add more convert 3d array  test cases

--- test_array3d.py
+++ test_array3d.py
@@ -16,10 +16,31 @@ def features():
 
 
 def test_simple():
+    '''At least this will work!'''
     assert True
-    return
 
 
 def test_2d_non_rolling(labels):
-    two_dim = a3d.create_2d_target_array(labels, 10, True)
-    assert two_dim[0] == labels[9]
+    time_step = 10
+    two_dim = a3d.create_2d_target_array(labels, time_step, True)
+    assert two_dim[0] == labels[time_step-1]
+
+
+def test_2d_odd_non_rolling(labels):
+    time_step = 8
+    data = np.copy(labels)
+    two_dim = a3d.create_2d_target_array(labels, time_step, True)
+    # labels = a3d.check_array_time_step_divisible(labels, time_step)
+    assert two_dim[0] == data[time_step-1]
+
+
+def test_2d_rolling(labels):
+    time_step = 10
+    two_dim = a3d.create_2d_target_array(labels, time_step, False)
+    assert two_dim[0] == labels[time_step-1]
+
+
+def test_3d_rolling(features, labels):
+    time_step = 10
+    three_dim = a3d.convert_3d_array(features, time_step, features.shape[1], False)
+    assert three_dim[0, -1, -1] == labels[time_step-1]
